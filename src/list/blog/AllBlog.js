88436a857c5eea74fadc2c2a2d0afa92f8a74be7
Poojagoyal99 <goyalpooja9902@gmail.com>
2024-08-10 10:17:28 +0530

    added

--- src/list/blog/AllBlog.js
+++ src/list/blog/AllBlog.js
@@ -1,3 +1,5 @@
+import React from "react";
+import { Link } from 'react-router-dom';
 import blog1 from "../../images/blog1.png";
 import blog2 from "../../images/blog2.png";
 import blog3 from "../../images/blog3.png";
@@ -6,15 +8,14 @@ import blog5 from "../../images/blog5.png";
 import blog6 from "../../images/blog6.png";
 import blog11 from "../../images/blog11.png";
 import blog12 from "../../images/blog12.png";
-import { Link } from 'react-router-dom';
 
 const AllBlog = () => {
   return (
     <>
-      <h1 className="font-semibold text-2xl mx-36">Recent Blog Posts</h1>
-      <div className="flex space-x-6 mt-10">
-        <Link to='/postdetails' className="ml-36 w-2/6 ">
-          <img src={blog11} alt="blogImage"></img>
+      <h1 className="font-semibold text-2xl mx-4 lg:mx-36">Recent Blog Posts</h1>
+      <div className="flex flex-col lg:flex-row space-y-6 lg:space-x-6 mt-10 lg:mx-36">
+        <Link to='/postdetails' className="lg:w-1/3">
+          <img src={blog11} alt="blogImage" className="w-full"></img>
           <p className="font-bold text-purple-600 pt-3">Name 1 Jan 2023</p>
           <h1 className="font-bold text-xl pt-3">Ujjain</h1>
           <p className="text-gray-500 pt-3">
@@ -22,11 +23,11 @@ const AllBlog = () => {
             Bowl winning Dynasty?
           </p>
         </Link>
-        <Link to='/postdetails' className="mr-36 flex space-x-4">
-          <div>
-            <img src={blog12} alt="blogImage" className="h-80"></img>
+        <Link to='/postdetails' className="flex flex-col lg:flex-row lg:w-2/3 space-y-4 lg:space-y-0 lg:space-x-4">
+          <div className="lg:w-1/2">
+            <img src={blog12} alt="blogImage" className="w-full h-80 object-cover"></img>
           </div>
-          <div className="w-48 mr-32">
+          <div className="lg:w-1/2">
             <p className="font-bold text-purple-600 pt-3">Name 1 Jan 2023</p>
             <h1 className="font-bold text-xl pt-3">
               Bill Walsh leadership lessons
@@ -35,145 +36,42 @@ const AllBlog = () => {
               Like to know the secrets of transforming a 2-14 team into a 3x
               Super Bowl winning Dynasty?
             </p>
-            <div className="flex pt-3">
+            <div className="flex pt-3 space-x-3">
               <button className="bg-purple-100 w-28 text-purple-500 h-8 rounded-full font-semibold">
                 Design
               </button>
-              <button className="bg-pink-100 text-pink-700 w-28 rounded-full h-8 ml-3 font-semibold">
+              <button className="bg-pink-100 text-pink-700 w-28 rounded-full h-8 font-semibold">
                 Research
               </button>
             </div>
           </div>
         </Link>
       </div>
-      <h1 className="font-semibold text-2xl mx-36 mt-10">All Blog Posts</h1>
-      <div>
-        <div className="flex mt-10 space-x-4 mr-32">
-          <Link to='/postdetails' className="ml-36">
-            <img src={blog1} alt="blogImage"></img>
-            <p className="font-bold text-purple-600 pt-3">Name 1 Jan 2023</p>
-            <h1 className="font-bold text-xl pt-3">
-              Bill Walsh leadership lessons
-            </h1>
-            <p className="text-gray-500 pt-3">
+
+      <h1 className="font-semibold text-2xl mx-4 lg:mx-36 mt-10">All Blog Posts</h1>
+      <div className="grid grid-cols-1  lg:grid-cols-2 xl:grid-cols-3 gap-4 mt-10 lg:mx-36">
+        {[blog1, blog2, blog3, blog4, blog5, blog6].map((blog, index) => (
+          <Link to='/postdetails' key={index} className="flex flex-col space-y-3">
+            <img src={blog} alt={`blogImage${index + 1}`} className="w-full mt-10"></img>
+            <p className="font-bold text-purple-600">Name 1 Jan 2023</p>
+            <h1 className="font-bold text-xl">Bill Walsh leadership lessons</h1>
+            <p className="text-gray-500">
               Like to know the secrets of transforming a 2-14 team into a 3x
               Super Bowl winning Dynasty?
             </p>
-            <div className="flex pt-3">
-              <button className="bg-purple-100 w-28 text-purple-500 h-8 rounded-full font-semibold">
-                Leadership
-              </button>
-              <button className="bg-gray-100 text-gray-700 w-28 rounded-full h-8 ml-3 font-semibold">
-                Management
-              </button>
-            </div>
-          </Link>
-          <Link to='/postdetails'>
-            <img src={blog2} alt="blogImage"></img>
-            <p className="font-bold text-purple-600 pt-3">Name 1 Jan 2023</p>
-            <h1 className="font-bold text-xl pt-3">
-              Bill Walsh leadership lessons
-            </h1>
-            <p className="text-gray-500 pt-3">
-              Like to know the secrets of transforming a 2-14 team into a 3x
-              Super Bowl winning Dynasty?
-            </p>
-            <div className="flex pt-3">
-              <button className="bg-teal-100 w-28 text-teal-600 h-8 rounded-full font-semibold">
-                Product
-              </button>
-              <button className="bg-blue-100 text-blue-700 w-28 rounded-full h-8 ml-3 font-semibold">
-                Research
-              </button>
-              <button className="bg-red-100 text-red-600 w-28 rounded-full h-8 ml-3 font-semibold">
-                Frameworks
-              </button>
-            </div>
-          </Link>
-          <Link to='/postdetails' className="mr-36">
-            <img src={blog3} alt="blogImage"></img>
-            <p className="font-bold text-purple-600 pt-3">Name 1 Jan 2023</p>
-            <h1 className="font-bold text-xl pt-3">
-              Bill Walsh leadership lessons
-            </h1>
-            <p className="text-gray-500 pt-3">
-              Like to know the secrets of transforming a 2-14 team into a 3x
-              Super Bowl winning Dynasty?
-            </p>
-            <div className="flex pt-3">
-              <button className="bg-purple-100 w-28 text-purple-500 h-8 rounded-full font-semibold">
+            <div className="flex pt-3 space-x-3 flex-wrap">
+              <button className="bg-purple-100 min-w-28 text-purple-500 h-8 rounded-full font-semibold">
                 Design
               </button>
-              <button className="bg-blue-100 text-blue-700 w-28 rounded-full h-8 ml-3 font-semibold">
+              <button className="bg-gray-100 text-gray-700 min-w-28 h-8 rounded-full font-semibold">
                 Research
               </button>
             </div>
           </Link>
-        </div>
-        <div className="flex mt-20 space-x-4 mr-32">
-          <Link to='/postdetails' className="ml-36">
-            <img src={blog4} alt="blogImage"></img>
-            <p className="font-bold text-purple-600 pt-3">Name 1 Jan 2023</p>
-            <h1 className="font-bold text-xl pt-3">
-              Bill Walsh leadership lessons
-            </h1>
-            <p className="text-gray-500 pt-3">
-              Like to know the secrets of transforming a 2-14 team into a 3x
-              Super Bowl winning Dynasty?
-            </p>
-            <div className="flex pt-3">
-              <button className="bg-purple-100 w-28 text-purple-500 h-8 rounded-full font-semibold">
-                Design
-              </button>
-              <button className="bg-blue-100 text-blue-700 w-28 rounded-full h-8 ml-3 font-semibold">
-                Research
-              </button>
-            </div>
-          </Link>
-          <Link to='/postdetails'>
-            <img src={blog5} alt="blogImage"></img>
-            <p className="font-bold text-purple-600 pt-3">Name 1 Jan 2023</p>
-            <h1 className="font-bold text-xl pt-3">
-              Bill Walsh leadership lessons
-            </h1>
-            <p className="text-gray-500 pt-3">
-              Like to know the secrets of transforming a 2-14 team into a 3x
-              Super Bowl winning Dynasty?
-            </p>
-            <div className="flex pt-3">
-              <button className="bg-green-100 min-w-28 text-green-600 min-h-8 rounded-full font-semibold">
-                Software Development
-              </button>
-              <button className="bg-pink-100 text-pink-700 w-28 rounded-full h-8 ml-3 font-semibold">
-                Tools
-              </button>
-              <button className="bg-red-100 text-red-600 w-28 rounded-full h-8 ml-3 font-semibold">
-                SaaS
-              </button>
-            </div>
-          </Link>
-          <Link to='/postdetails' className="mr-36">
-            <img src={blog6} alt="blogImage"></img>
-            <p className="font-bold text-purple-600 pt-3">Name 1 Jan 2023</p>
-            <h1 className="font-bold text-xl pt-3">
-              Bill Walsh leadership lessons
-            </h1>
-            <p className="text-gray-500 pt-3">
-              Like to know the secrets of transforming a 2-14 team into a 3x
-              Super Bowl winning Dynasty?
-            </p>
-            <div className="flex pt-3">
-              <button className="bg-purple-100 w-28 text-purple-500 h-8 rounded-full font-semibold">
-                Podcast
-              </button>
-              <button className="bg-gray-100 text-gray-700 w-36 rounded-full h-8 ml-3 font-semibold">
-                Customer Success
-              </button>
-            </div>
-          </Link>
-        </div>
+        ))}
       </div>
-      <div className="bg-slate-200 w-5/6 ml-36  h-px mt-10 "></div>
+
+      <div className="bg-slate-200 w-5/6 mx-auto h-px mt-10"></div>
       <div className="flex justify-evenly mt-5">
         <div>
           <p className="text-gray-500">Previous</p>
