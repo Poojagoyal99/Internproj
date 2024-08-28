a49f937240d20025cc54cbd20ed7c84034c72278
Poojagoyal99 <goyalpooja9902@gmail.com>
2024-08-28 14:33:03 +0530

    added

--- src/list/rentals/BookingCards.js
+++ src/list/rentals/BookingCards.js
@@ -4,63 +4,101 @@ import { useNavigate } from 'react-router-dom';
 import seatImg from "../../images/seat.png";
 import bag from "../../images/luggage.png";
 import speed from "../../images/speedometer.png";
+
 function BookingCards() {
-    const navigate = useNavigate();
-    const navigateToAboutInfo = () => {
-        navigate('/yourInfo');
-    };
-    return(
-        <div className="h-full w-full flex space-x-16 my-24 ">
-<div className="ml-24  h-96 w-96 bg-customCream rounded-lg">
-    <h1 className="text-xl font-bold text-center pt-3">Toyota Matrix</h1>
-<img src = {carImg} alt='carimage' className="h-36 w-76 ml-14 my-5"></img>
-<div className="flex flex-row mx-14 my-4 space-x-10">
-    <div className="h-16 w-16 rounded-full border-2 border-white bg-gray-200"><img src={speed} alt="seatimag" className="h-8 ml-4 mt-3 w-8" />
-    <p className="mt-5 text-sm w-20">120 <span className="text-gray-500">km/hr</span></p></div>
-    <div className="h-16 w-16 rounded-full border-2 border-white bg-gray-200"><img src={seatImg} alt="seatimag" className="h-8 ml-4 mt-3 w-8"></img> 
-    <p className="mt-5 text-sm w-20">8<span className="text-gray-500"> seater</span></p> </div>
-    <div className="h-16 w-16 rounded-full border-2 border-white bg-gray-200"> <img src={bag} alt="seatimag" className="h-8 ml-4 mt-3 w-8"></img>
-    <p className="mt-5 text-sm w-20">10<span className="text-gray-500"> laguage</span></p>
+  const navigate = useNavigate();
+  const navigateToAboutInfo = () => {
+    navigate('/yourInfo');
+  };
+
+  return (
+    <div className="h-full w-full flex flex-col lg:flex-row lg:space-x-8 space-y-8 lg:space-y-0 my-16 px-4 lg:px-24">
+      <div className="h-96 w-full lg:w-96 bg-customCream rounded-lg">
+        <h1 className="text-xl font-bold text-center pt-3">Toyota Matrix</h1>
+        <img src={carImg} alt="carimage" className="h-36 w-76 mx-auto my-5" />
+        <div className="flex justify-around mx-4 lg:mx-8 my-4 space-x-4 lg:space-x-10">
+          <div className="h-16 w-16 rounded-full border-2 border-white bg-gray-200 flex flex-col items-center justify-center">
+            <img src={speed} alt="speed" className="h-8 w-8 mt-9 mb-2" />
+            <p className="text-sm w-20 text-center mt-3 ">120 <span className="text-gray-500">km/hr</span></p>
+          </div>
+          <div className="h-16 w-16 rounded-full border-2 border-white bg-gray-200 flex flex-col items-center justify-center">
+            <img src={seatImg} alt="seat" className="h-8 w-8 mb-2 mt-9" />
+            <p className="text-sm w-20 text-center mt-3">8<span className="text-gray-500"> seater</span></p>
+          </div>
+          <div className="h-16 w-16 rounded-full border-2 border-white bg-gray-200 flex flex-col items-center justify-center">
+            <img src={bag} alt="luggage" className="h-8 w-8 mb-2 mt-9" />
+            <p className="text-sm w-20 text-center mt-3">10<span className="text-gray-500"> luggage</span></p>
+          </div>
+        </div>
+        <div className="h-12 w-5/6 lg:w-80 bg-gray-50 rounded-lg mx-auto my-4 mt-7 flex items-center justify-between px-4">
+          <p className="text-xl font-medium">Rs.2000<span className="text-base text-gray-500">/day</span></p>
+          <button
+            className="h-8 w-28 lg:w-36 rounded-xl text-white text-xl font-thin bg-customPeach"
+            onClick={navigateToAboutInfo} >
+            BOOK NOW
+          </button>
+        </div>
+      </div>
+
+      {/* Repeat the same structure for additional cards */}
+      <div className="h-96 w-full lg:w-96 bg-customCream rounded-lg">
+        <h1 className="text-xl font-bold text-center pt-3">Toyota Matrix</h1>
+        <img src={carImg} alt="carimage" className="h-36 w-76 mx-auto my-5" />
+        <div className="flex justify-around mx-4 lg:mx-8 my-4 space-x-4 lg:space-x-10">
+          <div className="h-16 w-16 rounded-full border-2 border-white bg-gray-200 flex flex-col items-center justify-center">
+            <img src={speed} alt="speed" className="h-8 w-8 mb-2 mt-9" />
+            <p className="text-sm w-20 text-center mt-3">120 <span className="text-gray-500">km/hr</span></p>
+          </div>
+          <div className="h-16 w-16 rounded-full border-2 border-white bg-gray-200 flex flex-col items-center justify-center">
+            <img src={seatImg} alt="seat" className="h-8 w-8 mb-2 mt-9" />
+            <p className="text-sm w-20 text-center mt-3">8<span className="text-gray-500"> seater</span></p>
+          </div>
+          <div className="h-16 w-16 rounded-full border-2 border-white bg-gray-200 flex flex-col items-center justify-center">
+            <img src={bag} alt="luggage" className="h-8 w-8 mb-2 mt-9" />
+            <p className="text-sm w-20 text-center mt-3">10<span className="text-gray-500"> luggage</span></p>
+          </div>
+        </div>
+        <div className="h-12 w-5/6 lg:w-80 bg-gray-50 rounded-lg mx-auto my-4 mt-7 flex items-center justify-between px-4">
+          <p className="text-xl font-medium">Rs.2000<span className="text-base text-gray-500">/day</span></p>
+          <button
+            className="h-8 w-28 lg:w-36 rounded-xl text-white text-xl font-thin bg-customPeach"
+            onClick={navigateToAboutInfo}
+          >
+            BOOK NOW
+          </button>
+        </div>
+      </div>
+
+      {/* Another card */}
+      <div className="h-96 w-full lg:w-96 bg-customCream rounded-lg">
+        <h1 className="text-xl font-bold text-center pt-3">Toyota Matrix</h1>
+        <img src={carImg} alt="carimage" className="h-36 w-76 mx-auto my-5" />
+        <div className="flex justify-around mx-4 lg:mx-8 my-4 space-x-4 lg:space-x-10">
+          <div className="h-16 w-16 rounded-full border-2 border-white bg-gray-200 flex flex-col items-center justify-center">
+            <img src={speed} alt="speed" className="h-8 w-8 mb-2 mt-9" />
+            <p className="text-sm w-20 text-center mt-3 ">120 <span className="text-gray-500">km/hr</span></p>
+          </div>
+          <div className="h-16 w-16 rounded-full border-2 border-white bg-gray-200 flex flex-col items-center justify-center">
+            <img src={seatImg} alt="seat" className="h-8 w-8 mb-2 mt-9" />
+            <p className="text-sm w-20 text-center mt-3">8<span className="text-gray-500"> seater</span></p>
+          </div>
+          <div className="h-16 w-16 rounded-full border-2 border-white bg-gray-200 flex flex-col items-center justify-center">
+            <img src={bag} alt="luggage" className="h-8 w-8 mb-2 mt-9" />
+            <p className="text-sm w-20 text-center mt-3">10<span className="text-gray-500"> luggage</span></p>
+          </div>
+        </div>
+        <div className="h-12 w-5/6 lg:w-80 bg-gray-50 rounded-lg mx-auto my-4 mt-7 flex items-center justify-between px-4">
+          <p className="text-xl font-medium">Rs.2000<span className="text-base text-gray-500">/day</span></p>
+          <button
+            className="h-8 w-28 lg:w-36 rounded-xl text-white text-xl font-thin bg-customPeach"
+            onClick={navigateToAboutInfo}
+          >
+            BOOK NOW
+          </button>
+        </div>
+      </div>
     </div>
-</div>
-    <div className="h-12 w-80 bg-gray-50 rounded-lg mx-8 my-4 mt-7">
-        <p className="text-xl font-medium pt-2 pl-5">Rs.2000<span className="text-base text-gray-500">/day</span> <button className="h-8 w-36 ml-5 rounded-xl text-white text-xl font-thin bg-customPeach" onClick={navigateToAboutInfo}>BOOK NOW</button></p>
-        </div>
-</div>
-<div className="ml-24  h-96 w-96 bg-customCream rounded-lg">
-    <h1 className="text-xl font-bold text-center pt-3">Toyota Matrix</h1>
-<img src = {carImg} alt='carimage' className="h-36 w-76 ml-14 my-5"></img>
-<div className="flex flex-row mx-14 my-4 space-x-10">
-    <div className="h-16 w-16 rounded-full border-2 border-white bg-gray-200"><img src={speed} alt="seatimag" className="h-8 ml-4 mt-3 w-8" />
-    <p className="mt-5 text-sm w-20">120 <span className="text-gray-500">km/hr</span></p></div>
-    <div className="h-16 w-16 rounded-full border-2 border-white bg-gray-200"><img src={seatImg} alt="seatimag" className="h-8 ml-4 mt-3 w-8"></img> 
-    <p className="mt-5 text-sm w-20">8<span className="text-gray-500"> seater</span></p> </div>
-    <div className="h-16 w-16 rounded-full border-2 border-white bg-gray-200"> <img src={bag} alt="seatimag" className="h-8 ml-4 mt-3 w-8"></img>
-    <p className="mt-5 text-sm w-20">10<span className="text-gray-500"> laguage</span></p>
-    </div>
-</div>
-    <div className="h-12 w-80 bg-gray-50 rounded-lg mx-8 my-4 mt-7">
-        <p className="text-xl font-medium pt-2 pl-5">Rs.2000<span className="text-base text-gray-500">/day</span> <button className="h-8 w-36 ml-5 rounded-xl text-white text-xl font-thin bg-customPeach" onClick={navigateToAboutInfo}>BOOK NOW</button></p>
-        </div>
-</div>
-<div className="ml-24  h-96 w-96 bg-customCream rounded-lg">
-    <h1 className="text-xl font-bold text-center pt-3">Toyota Matrix</h1>
-<img src = {carImg} alt='carimage' className="h-36 w-76 ml-14 my-5"></img>
-<div className="flex flex-row mx-14 my-4 space-x-10">
-    <div className="h-16 w-16 rounded-full border-2 border-white bg-gray-200"><img src={speed} alt="seatimag" className="h-8 ml-4 mt-3 w-8" />
-    <p className="mt-5 text-sm w-20">120 <span className="text-gray-500">km/hr</span></p></div>
-    <div className="h-16 w-16 rounded-full border-2 border-white bg-gray-200"><img src={seatImg} alt="seatimag" className="h-8 ml-4 mt-3 w-8"></img> 
-    <p className="mt-5 text-sm w-20">8<span className="text-gray-500"> seater</span></p> </div>
-    <div className="h-16 w-16 rounded-full border-2 border-white bg-gray-200"> <img src={bag} alt="seatimag" className="h-8 ml-4 mt-3 w-8"></img>
-    <p className="mt-5 text-sm w-20">10<span className="text-gray-500"> laguage</span></p>
-    </div>
-</div>
-    <div className="h-12 w-80 bg-gray-50 rounded-lg mx-8 my-4 mt-7">
-        <p className="text-xl font-medium pt-2 pl-5">Rs.2000<span className="text-base text-gray-500">/day</span> <button className="h-8 w-36 ml-5 rounded-xl text-white text-xl font-thin bg-customPeach" onClick={navigateToAboutInfo}>BOOK NOW</button></p>
-        </div>
-</div>
-        </div>
-    );
-};
+  );
+}
 
 export default BookingCards;
--- src/list/rentals/SearchingBar.js
+++ src/list/rentals/SearchingBar.js
@@ -1,65 +1,67 @@
-import { FaCalendarAlt, FaUser } from 'react-icons/fa';
+import { FaUser } from 'react-icons/fa';
+
 function SearchingBar() {
-    return(
-        <>
-        <div className="w-full h-full">
-        <div className="bg-gradient-to-r from-fed8bc to-ffbe82 w-9/12 h-32 mx-44 mt-10 rounded-3xl flex justify-evenly">
-        <div>
-            <h1 className='mt-6 font-bold text-2xl'>Book a Taxi</h1>
-            <p className='text-lg text-gray-600 mt-2'>Discover the perfect ride for you!</p>
-        </div>
-        <div className="flex justify-around">
-        <div className="flex mb-4 space-x-6 mt-5">
-                <div className='w-32'>
-                  <label className="block text-black text-sm font-semibold mb-2" htmlFor="checkin-date">
-                    Pick Up Date
-                  </label>
-                  <div className="relative">
-                    <input
-                      type="date"
-                      id="checkin-date"
-                      className="bg-white text-black w-full py-2 px-1 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 appearance-none"
-                    />
-                  </div>
-                </div>
-                <div className="w-32">
-                  <label className="block text-black text-sm font-semibold mb-2" htmlFor="checkout-date">
-                    Drop Date
-                  </label>
-                  <div className="relative">
-                    <input
-                      type="date"
-                      id="checkout-date"
-                       className="bg-white text-black w-full py-2  px-1 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 appearance-none"
-                    />
-                  </div>
-                </div>
-                <div className="w-28">
-                  <label className="block text-black text-sm font-semibold mb-2" htmlFor="adults">
-                    Persons
-                  </label>
-                  <div className="relative">
-                    <span className="absolute inset-y-0 left-0 pl-3 flex items-center text-black">
-                      <FaUser />
-                    </span>
-                    <input
-                      type="number"
-                      id="adults"
-                      className="bg-white text-black w-full py-2 pl-10 pr-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 appearance-none"
-                      min="1"
-                      defaultValue=""
-                      style={{ backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'%3E%3Cpath fill='black' d='M7 10l5 5 5-5z'/%3E%3C/svg%3E")`, backgroundRepeat: 'no-repeat', backgroundPosition: 'right 0.75rem center', backgroundSize: '1.5rem 1.5rem' }}
-                    />
-                  </div>
-                </div>
+  return (
+    <>
+      <div className="w-full h-full flex justify-center">
+        <div className="bg-gradient-to-r from-fed8bc to-ffbe82 w-full lg:w-9/12 max-w-screen-lg h-auto lg:h-32 mx-4 lg:mx-auto mt-10 rounded-3xl flex flex-col lg:flex-row justify-evenly items-center p-4 lg:p-0 space-y-4 lg:space-y-0 lg:space-x-6">
+          
+          <div className="text-center lg:text-left lg:w-1/3 ml-4">
+            <h1 className="font-bold text-xl lg:text-2xl">Book a Taxi</h1>
+            <p className="text-md lg:text-lg text-gray-600 mt-2">Discover the perfect ride for you!</p>
+          </div>
+
+          <div className="flex flex-col lg:flex-row justify-around items-center lg:items-end w-full lg:w-2/3 space-y-4 lg:space-y-0 lg:space-x-6">
+            <div className="w-full lg:w-32">
+              <label className="block text-black text-sm font-semibold mb-2" htmlFor="checkin-date">
+                Pick Up Date
+              </label>
+              <input
+                type="date"
+                id="checkin-date"
+                className="bg-white text-black w-full py-2 px-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
+              />
+            </div>
+
+            <div className="w-full lg:w-32">
+              <label className="block text-black text-sm font-semibold mb-2" htmlFor="checkout-date">
+                Drop Date
+              </label>
+              <input
+                type="date"
+                id="checkout-date"
+                className="bg-white text-black w-full py-2 px-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
+              />
+            </div>
+
+            <div className="w-full lg:w-28">
+              <label className="block text-black text-sm font-semibold mb-2" htmlFor="adults">
+                Persons
+              </label>
+              <div className="relative">
+                <span className="absolute inset-y-0 left-0 pl-3 flex items-center text-black">
+                  <FaUser />
+                </span>
+                <input
+                  type="number"
+                  id="adults"
+                  className="bg-white text-black w-full py-2 pl-10 pr-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                  min="1"
+                  defaultValue=""
+                />
               </div>
-              </div>
-        <div>
-            <button className='mt-11 w-32 h-12 text-white bg-customPeach  rounded-xl font-bold text-lg shadow-xl'>SEARCH</button>
+            </div>
+          </div>
+
+          <div className="w-full lg:w-auto ">
+            <button className="w-full lg:w-32 h-12 text-white bg-customPeach rounded-xl font-bold text-lg shadow-xl mr-5">
+              SEARCH
+            </button>
+          </div>
         </div>
-        </div>
-        </div>
-        </>
-    );
-};
+      </div>
+    </>
+  );
+}
+
 export default SearchingBar;
